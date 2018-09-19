17ce2f496e222a14021e6c3081036a490ca850fc
Eduard <lepel1999@mail.ru>
2018-09-19 08:59:58 +0300

    final commit

--- client-swarm.js
+++ client-swarm.js
@@ -1,15 +1,8 @@
 const exec = require('child_process');
-var spawn = require('child_process').spawn,
-    grep  = spawn('grep', ['ssh']);
 
 var threadClient = function startClient(count) {
   for(var i = 0; i < count - 1; i++){
-    exec.fork('./client.js');
-
-    grep.on('exit', function (code, signal) {
-      console.log('child process terminated due to receipt of signal '+signal);
-    })
-    grep.kill('SIGHUP');
+    exec.fork('./client.js')
   }
 };
 
